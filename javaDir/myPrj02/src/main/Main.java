51665d1653f3aa74b8f7778c0cf602c7f5b8ecf4
DESKTOP-P64FJAV\user1 <dydwls8369@gmail.com>
2024-03-15 15:04:54 +0900

    수정했어..
    이제집에가도 될까??

--- javaDir/myPrj02/src/main/Main.java
+++ javaDir/myPrj02/src/main/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("두번째 프로젝트");
+		System.out.println("변수");
+		System.out.println("제어문");
+		System.out.println("배열");
+		
 	}
 
 }
